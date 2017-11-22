1bdcabfccc8f9b462a6a52146ab71cb0ebce447f
Santino <santiisgro96@gmail.com>
2017-11-22 20:39:20 +0100

    Get input from user and check if exist in title
    Signed-off-by: Santino <santiisgro96@gmail.com>

--- TNT_Notify.py
+++ TNT_Notify.py
@@ -4,6 +4,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import requests
+import re
 from pprint import pprint
 from xml.etree import ElementTree
 
@@ -13,12 +14,18 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-def start(bot, update):
-    print("Ciao")
+def start(bot, update):    
     update.message.reply_text('Insert /set <Name> to insert a new serie')
 
-
-
+def set(bot, update, args, job_queue, chat_data):
+    chat_id = update.message.chat_id
+    r = requests.get('http://www.tntvillage.scambioetico.org/rss.php?c=29&p=40')
+    if(r.status_code == requests.codes.ok):
+        tree = ElementTree.fromstring(r.content)
+        for neighbor in tree.iter('item'):
+            args = [re.sub('[^0-9a-zA-Z]+', '', x.lower()) for x in args]
+            if all(ext in re.sub('[^0-9a-zA-Z]+', '',neighbor.find('title').text.lower()) for ext in args):
+                print(neighbor.find('title').text.encode("utf8"))
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
@@ -46,11 +53,3 @@ if __name__ == '__main__':
     main()
 
 
-''' 
-
-r = requests.get('http://www.tntvillage.scambioetico.org/rss.php?c=29&p=40')
-if(r.status_code == requests.codes.ok):
-	tree = ElementTree.fromstring(r.content)
-	for neighbor in tree.iter('item'):
-		print(neighbor.find('title').text)
-'''
