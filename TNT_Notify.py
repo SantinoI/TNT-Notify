6f71b9ef4ac429762feafaa2df09767343028b72
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-11-27 20:53:19 +0100

    Add more try, exception and control case. Print the torrent info. Define last command and write a little help message text

--- TNT_Notify.py
+++ TNT_Notify.py
@@ -12,45 +12,71 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 TOKEN = open('token.conf', 'r').read().replace("\n", "")
+LINK = "http://www.tntvillage.scambioetico.org/rss.php?c=29&p="
 
 logger = logging.getLogger(__name__)
 
-def start(bot, update):    
-    update.message.reply_text('Insert /set <Name> to insert a new serie')
+def help(bot, update):    
+    update.message.reply_text('Use /set <name> to insert a new serie\nUse /unset to show the list of your series and delete once\nUse /last <number> to show the last number series uploaded')
 
 def set(bot, update, args, job_queue, chat_data):
-    chat_id = update.message.chat_id
-    name = [y for y in [re.sub('[^0-9a-zA-Z]+', '', x.lower()) for x in args] if y]    
-    
-    if not chat_data['job']:   
+    if len(args) >= 1:
+        chat_id = update.message.chat_id
+        name = [y for y in [re.sub('[^0-9a-zA-Z]+', '', x.lower()) for x in args] if y]    
+        
+        '''
+        if 'job' in chat_data:   
+            job = [
+                job_queue.run_daily(lambda bot, job: check(bot, job, chat_data), datetime.time(9, 00, 00),  context=chat_id, name='At 09:00'),
+                job_queue.run_daily(lambda bot, job: check(bot, job, chat_data), datetime.time(11, 00, 00), context=chat_id, name='At 11:00'),
+                job_queue.run_daily(lambda bot, job: check(bot, job, chat_data), datetime.time(15, 00, 00), context=chat_id, name='At 15:00'),
+                job_queue.run_daily(lambda bot, job: check(bot, job, chat_data), datetime.time(18, 00, 00), context=chat_id, name='At 18:00'),
+                job_queue.run_daily(lambda bot, job: check(bot, job, chat_data), datetime.time(20, 00, 00), context=chat_id, name='At 20:00')
+            ]
+            chat_data['job'] = job
+        '''
+        #For testing
         job = [
-            job_queue.run_daily(lambda bot, job: check(bot, job, chat_data), datetime.time(9, 00, 00),  context=chat_id, name='At 09:00'),
-            job_queue.run_daily(lambda bot, job: check(bot, job, chat_data), datetime.time(11, 00, 00), context=chat_id, name='At 11:00'),
-            job_queue.run_daily(lambda bot, job: check(bot, job, chat_data), datetime.time(15, 00, 00), context=chat_id, name='At 15:00'),
-            job_queue.run_daily(lambda bot, job: check(bot, job, chat_data), datetime.time(18, 00, 00), context=chat_id, name='At 18:00'),
-            job_queue.run_daily(lambda bot, job: check(bot, job, chat_data), datetime.time(20, 00, 00), context=chat_id, name='At 20:00')
+            job_queue.run_repeating(lambda bot, job: check(bot, job, chat_data), 10, context=chat_id, name='At 09:00')
         ]
         chat_data['job'] = job
-    '''
-    #For testing
-    job = [
-        job_queue.run_repeating(lambda bot, job: check(bot, job, chat_data), 10, context=chat_id, name='At 09:00')
-    ]
-    chat_data['job'] = job
-    '''
+    
 
-    chat_data[" ".join(name)] = {
-        "title": name,
-        "lastNotify": [],
-        "originalName": " ".join(args)
-    }
+        chat_data[" ".join(name)] = {
+            "title": name,
+            "lastNotify": [],
+            "originalName": " ".join(args)
+        }
 
-    update.message.reply_text('Serie successfully set!')
+        update.message.reply_text('Serie successfully set!')
+    else:
+        update.message.reply_text('Usage: /set <name>')
+
+def last(bot, update, args):
+    try:
+        times = args[0]
+        if int(times) >= 1 and int(times) <= 80 :
+            r = requests.get(LINK + times)
+            if(r.status_code == 200):
+                tree = ElementTree.fromstring(r.content)
+                for neighbor in tree.iter('item'):
+                    title = neighbor.find('title').text
+                    link = neighbor.find('enclosure').get('url')
+                    descr = neighbor.find('description').text.split("<BR>")
+                    description = ""
+                    for d in descr:	
+                        if "torrent data" in d.lower():
+                            description = d.split(":")[-1].strip() 
+                    bot.send_message(update.message.chat_id, text="<b>Serie TV:</b>\n{}\n<b>Info:</b>\n{}\n<a href='{}'>Link Torrent</a>".format(title, description, link), parse_mode="HTML")
+        else:
+            update.message.reply_text('The number must be between 1 and 80')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /last <number>')
 
 def check(bot, job, chat_data):
     for key, value in chat_data.items():
         if key != 'job':
-            r = requests.get('http://www.tntvillage.scambioetico.org/rss.php?c=29&p=40')
+            r = requests.get(LINK + "30")
             if(r.status_code == 200):
                 tree = ElementTree.fromstring(r.content)
                 for neighbor in tree.iter('item'):
@@ -58,7 +84,12 @@ def check(bot, job, chat_data):
                     if not title in value["lastNotify"]:
                         if all(ext in re.sub('[^0-9a-zA-Z]+', '', title.lower()) for ext in value["title"]):
                             link = neighbor.find('enclosure').get('url')
-                            bot.send_message(job.context, text="*Serie TV Trovata:*\n{}\n[Link Torrent]({})".format(title, link), parse_mode="Markdown")    
+                            descr = neighbor.find('description').text.split("<BR>")
+                            description = ""
+                            for d in descr:	
+                                if "torrent data" in d.lower():
+                                    description = d.split(":")[-1].strip() 
+                            bot.send_message(job.context, text="<b>Serie TV Found:</b>\n{}\n<b>Info:</b>\n{}\n<a href='{}'>Link Torrent</a>".format(title, description, link), parse_mode="HTML")
                             torRequest = requests.get(link)
                             if(torRequest.status_code == 200):
                                 fileName = torRequest.headers.get('Content-Disposition').split(";")[1].strip().split("=")[1].strip().replace('"','')
@@ -69,43 +100,42 @@ def check(bot, job, chat_data):
                                 value["lastNotify"].append(title)
 
 def unset(bot, update, chat_data):
-    keyboard = []
-    count = 0
-    for key, value in chat_data.items():
-        if key != 'job':
-            keyboard.append([InlineKeyboardButton(value["originalName"], callback_data = key)])
-    
-   
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+    if len(chat_data) >= 2:    
+        keyboard = []
+        for key, value in chat_data.items():
+            if key != 'job':
+                keyboard.append([InlineKeyboardButton(value["originalName"], callback_data=key)])
+        
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text('Select the serie to delete:', reply_markup=reply_markup)
+    else: 
+        update.message.reply_text("You haven't a serie. Use /set <name>")
 
 def button(bot, update, chat_data):
     query = update.callback_query
-    serieTv = chat_data[query.data]
-    originalName = serieTv["originalName"]
-    del chat_data[originalName]
+    del chat_data[query.data]
 
-    bot.edit_message_text(text="Serie tv cancellata: {}".format(query.data),
+    bot.edit_message_text(text="Serie TV Delete: {}".format(query.data),
                           chat_id=query.message.chat_id,
                           message_id=query.message.message_id)
     
-
-
+    if len(chat_data) == 1:
+        for job in chat_data['job']:
+            job.schedule_removal()
+        del chat_data['job']
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
-
 
 def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
     
-
-    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("start", help))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler('unset', unset, pass_chat_data=True))
     dp.add_handler(CallbackQueryHandler(button, pass_chat_data=True))
+    dp.add_handler(CommandHandler("last", last, pass_args=True))
     dp.add_handler(CommandHandler("set", set, pass_args=True, pass_job_queue=True, pass_chat_data=True))
 
     # log all errors
